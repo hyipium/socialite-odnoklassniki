63f2ca0b42e7ddeb32d744e1af9a9f813936e108
hyipium <hyipium@gmail.com>
2018-09-10 13:03:18 +0300

    update from https://github.com/SocialiteProviders/Odnoklassniki/blob/master/Provider.php

--- src/Provider.php
+++ src/Provider.php
@@ -2,9 +2,10 @@
 
 namespace JhaoDa\SocialiteProviders\Odnoklassniki;
 
+use Illuminate\Support\Arr;
 use Laravel\Socialite\Two\ProviderInterface;
-use SocialiteProviders\Manager\OAuth2\User;
 use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
 
 class Provider extends AbstractProvider implements ProviderInterface
 {
@@ -12,72 +13,59 @@ class Provider extends AbstractProvider implements ProviderInterface
      * Unique Provider Identifier.
      */
     const IDENTIFIER = 'ODNOKLASSNIKI';
-
     /**
      * {@inheritdoc}
      */
-    protected $scopes = ['GET_EMAIL'];
-
+    protected $scopes = ['VALUABLE_ACCESS', 'GET_EMAIL'];
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopeSeparator = ';';
     /**
      * {@inheritdoc}
      */
     protected function getAuthUrl($state)
     {
-        return $this->buildAuthUrlFromBase(
-            'http://connect.ok.ru/oauth/authorize', $state
-        );
+        return $this->buildAuthUrlFromBase('https://connect.ok.ru/oauth/authorize', $state);
     }
-
     /**
      * {@inheritdoc}
      */
     protected function getTokenUrl()
     {
-        return 'https://api.odnoklassniki.ru/oauth/token.do';
+        return 'https://api.ok.ru/oauth/token.do';
     }
-
     /**
      * {@inheritdoc}
      */
     protected function getUserByToken($token)
     {
-        $params = [
-            'format'          => 'json',
+        $secretKey = md5($token.$this->clientSecret);
+        $publicKey = app()['config']['services.odnoklassniki']['client_public'];
+        $sign = 'application_key='.$publicKey.'format=jsonmethod=users.getCurrentUser'.$secretKey;
+        $params = http_build_query([
             'method'          => 'users.getCurrentUser',
-            'application_key' => $this->getConfig('client_public', env('ODNOKLASSNIKI_PUBLIC')),
-            'fields'          => 'uid,name,first_name,last_name,birthday,pic190x190,has_email,email'
-        ];
-
-        ksort($params, SORT_STRING);
-
-        $_params = array_map(function($key, $value) {
-            return $key . '=' . $value;
-        }, array_keys($params), array_values($params));
-
-        $params['sig'] = md5(implode('', $_params) . md5($token . $this->clientSecret));
-        $params['access_token'] = $token;
-
-        $response = $this->getHttpClient()->get(
-            'https://api.ok.ru/fb.do?' . http_build_query($params)
-        );
-
-        return json_decode($response->getBody(), true);
+            'format'          => 'json',
+            'application_key' => $publicKey,
+            'sig'             => md5($sign),
+            'access_token'    => $token,
+        ]);
+        $response = $this->getHttpClient()->get('https://api.odnoklassniki.ru/fb.do?'.$params);
+        return json_decode($response->getBody()->getContents(), true);
     }
-
     /**
      * {@inheritdoc}
      */
     protected function mapUserToObject(array $user)
     {
         return (new User())->setRaw($user)->map([
-            'id'       => $user['uid'],
-            'name'     => $user['name'],
+            'id'       => Arr::get($user, 'uid'),
             'nickname' => null,
-            'email'    => array_get($user, 'email'),
-            'avatar'   => array_get($user, 'pic190x190'),
+            'name'     => Arr::get($user, 'name'),
+            'email'    => Arr::get($user, 'email'),
+            'avatar'   => Arr::get($user, 'pic_3'),
         ]);
     }
-
     /**
      * {@inheritdoc}
      */
@@ -86,12 +74,5 @@ class Provider extends AbstractProvider implements ProviderInterface
         return array_merge(parent::getTokenFields($code), [
             'grant_type' => 'authorization_code',
         ]);
-    }
-
-    public static function additionalConfigKeys()
-    {
-        return [
-            'client_public'
-        ];
     }
 }
